a10754bb567d35db604f8c16ca17ab2f3292fa27
Annisa Nabila Nasution <annisanabila14@gmail.com>
2020-06-06 16:02:10 +0700

    making save button

---
+++ Fit Journey/Pages/Diary/Post/Controller/PostController.swift
@@ -0,0 +1,18 @@
+//
+//  PostController.swift
+//  Fit Journey
+//
+//  Created by Annisa Nabila Nasution on 06/06/20.
+//  Copyright © 2020 Annisa Nabila Nasution. All rights reserved.
+//
+
+import UIKit
+
+class PostController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
